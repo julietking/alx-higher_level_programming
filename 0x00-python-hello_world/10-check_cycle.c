3ef87a586a4d61c896a9590a764c41ad765eafb5
julietking <ohagwajuliet6@gmail.com>
2022-09-21 11:21:13 -0700

    hotfix

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,23 +1,31 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * check_cycle - finds the index at which a loop starts
- * @head: list head
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
  *
- * Return: 0 if no cycle 1 if there is cycle
+ * Return: If there is no cycle - 0.
+ * If there is a cycle - 1.
  */
-int check_cycle(listint_t *head)
+int check_cycle(listint_t *list)
 {
-	listint_t *fast = head, *slow = head;
+	listint_t *slow, *fast;
 
-	if (!head || !(head->next))
+	if (list == NULL || list->next == NULL)
 		return (0);
+
+	slow = list->next;
+	fast = list->next->next;
+
 	while (slow && fast && fast->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
 		if (slow == fast)
 			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
 	}
+
 	return (0);
 }
